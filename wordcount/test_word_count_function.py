f0f76bd558fa05f09aafb62bdb08b6424acdd698
dupadhyay <dhirajpresent@gmail.com>
2020-04-30 13:12:34 -0400

    updated code

--- wordcount/test_word_count_function.py
+++ wordcount/test_word_count_function.py
@@ -3,4 +3,4 @@ from word_count_function import word_count
 
 def test_word_count_function(spark_context):
     counts = word_count(spark_context.parallelize(list("Hello World")))
-    assert counts == [('l', 3), ('o', 2), (' ', 1), ('e', 1), ('d', 1), ('H', 1), ('r', 1), ('W', 1)]
+    assert counts == [('l', 3), ('o', 2), ('H', 1), ('e', 1), (' ', 1), ('W', 1), ('r', 1), ('d', 1)]
